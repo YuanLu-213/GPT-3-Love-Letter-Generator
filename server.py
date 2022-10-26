d0a1be5f6bcc60c1f19ce68a1b539a40d252e771
Mia0802 <xl3129@columbia.edu>
2022-10-25 20:35:14 -0400

    template for generate paragraph and display result

--- server.py
+++ server.py
@@ -14,3 +14,12 @@ def home():
 def toWhoAndOccasion(key):
     key = key
     return render_template("basic.html", whom=key)
+
+
+@app.route("/generate_paragraph")
+def generate_paragraph():
+    return render_template("generate_paragraph.html")
+
+@app.route("/generated_result")
+def generated_result():
+    return render_template("generated_result.html")
